53066af7782431acac699209362cd0c0d17eb45a
Thomas Lockie <thomas_lockie@yahoo.com>
2026-07-02 23:38:20 +0100

    feat: Add threlte demo scene to confirm rendering

---
+++ src/lib/Scene.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { T, useTask } from '@threlte/core'
+  import { Spring } from 'svelte/motion'
+
+  const scale = new Spring(1)
+
+  let rotation = $state(0)
+  useTask((delta) => {
+    rotation += delta
+  })
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[10, 10, 10]}
+  oncreate={(ref) => {
+    ref.lookAt(0, 1, 0)
+  }}
+/>
+
+<T.DirectionalLight position={[0, 10, 10]} />
+
+<T.Mesh
+  rotation.y={rotation}
+  position.y={1}
+  scale={scale.current}
+  onpointerenter={() => {
+    scale.target = 1.5
+  }}
+  onpointerleave={() => {
+    scale.target = 1
+  }}
+>
+  <T.BoxGeometry args={[1, 2, 1]} />
+  <T.MeshStandardMaterial color="hotpink" />
+</T.Mesh>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { Canvas } from '@threlte/core'
+//   import { interactivity } from '@threlte/extras'
+  import Scene from '$lib/Scene.svelte'
+
+//   interactivity()
+</script>
+
+<Canvas>
+  <Scene />
+</Canvas>
